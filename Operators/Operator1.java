96b0d42d9ac4c42bc05ef689b8bcb810a2b9743d
Nishant Wagharalkar <nishantw1108@gmail.com>
2025-08-23 14:58:32 +0530

    Add Operators folder with Java programs

---
+++ Operators/Operator1.java
@@ -0,0 +1,26 @@
+class Operator1 {
+ public static void main(String[] args){
+   int result;
+   
+   // Addition
+   result = 10+20;
+   System.out.println("Addition is " + result);
+   
+   // Substraction
+   result = 100-20;
+   System.out.println("Substraction is " + result);
+   
+   // Multiplication
+   result = 10*20;
+   System.out.println("Multiplication is " + result);
+   
+   // Division
+   result = 10/2;
+   System.out.println("Division is " + result);
+   
+   // Modulus
+   result = 10%2;
+   System.out.println("Modulus is " + result);
+ }
+
+}
