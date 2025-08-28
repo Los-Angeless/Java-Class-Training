35d71ae77bdeca10f1b1102d45ceab84d06a98e2
Nishant Wagharalkar <139781560+Los-Angeless@users.noreply.github.com>
2025-08-28 18:48:26 +0530

    added Switch example

---
+++ Control-Flow-Statements/Switch/Switch1.java
@@ -0,0 +1,16 @@
+class Switch1
+{
+ public static void main(String[] args)
+ {
+  int a=2;
+  switch(a)
+  {
+   case 1 : System.out.println("Inside Case1");
+   break;
+   case 2 : System.out.println("Inside Case2");
+   break;
+   default : System.out.println("Inside default");
+   break;
+  }
+ }
+}
